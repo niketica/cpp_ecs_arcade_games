9b46ec626ef25faac2b73f29fd14c22d24dfc9a7
niketica <commerce.alexander.n@gmail.com>
2024-08-01 22:42:19 +0200

    Create tetrist grid + tetromino shapes

--- ECSArcade/src/game/scene/tetris/SceneTetris.cpp
+++ ECSArcade/src/game/scene/tetris/SceneTetris.cpp
@@ -7,7 +7,43 @@ SceneTetris::SceneTetris(Game* game) : Scene(game)
 
 void SceneTetris::init()
 {
+    game->getECSManager().reset();
+    srand(time(NULL));
     createActionList();
+    createNextTetromino();
+    activateNextTetromino();
+}
+
+void SceneTetris::createNextTetromino()
+{
+    Tetromino tetromino = tetrominos[rand() % nrOfTetrominos];
+    tetromino.topLeftPos = { (columns / 2) - ( shapeSize / 2), 0 };
+    tetromino.active = false;
+    tetromino.next = true;
+
+    EntityTag tag{ TETROMINO };
+
+    auto entity = game->getECSManager().addEntity();
+    game->getECSManager().addComponent<EntityTag>(entity, tag);
+    game->getECSManager().addComponent<Tetromino>(entity, tetromino);
+}
+
+void SceneTetris::activateNextTetromino()
+{
+    auto eTetrominos = game->getEntitiesWithTag(TETROMINO);
+
+    for (auto& entity : eTetrominos)
+    {
+        auto tetromino = game->getECSManager().getComponent<Tetromino>(entity);
+
+        if (!tetromino->next) continue;
+
+        tetromino->next = false;
+        tetromino->active = true;
+        break;
+    }
+
+    createNextTetromino();
 }
 
 void SceneTetris::createActionList()
@@ -51,6 +87,90 @@ void SceneTetris::render(sf::RenderWindow& window)
 {
     window.clear(windowClearColor);
 
+    renderBlocks(window);
+    renderGrid(window);
+
     window.display();
 }
 
+void SceneTetris::renderBlocks(sf::RenderWindow& window)
+{
+    auto eTetrominos = game->getEntitiesWithTag(TETROMINO);
+
+    for (auto& entity : eTetrominos)
+    {
+        auto tetromino = game->getECSManager().getComponent<Tetromino>(entity);
+
+        if (tetromino->next) continue;
+
+        auto xOffset = tetromino->topLeftPos.x;
+        auto yOffset = tetromino->topLeftPos.y;
+
+        auto shape = shapes[tetromino->shapeIndex];
+        for (int x = 0; x < shapeSize; x++)
+        {
+            for (int y = 0; y < shapeSize; y++)
+            {
+                if (shape[y][x] == 0) continue;
+
+                sf::RectangleShape block(sf::Vector2f(cellSize, cellSize));
+                block.setFillColor(tetromino->color);
+                block.setPosition((x + xOffset) * cellSize, (y + yOffset) * cellSize);
+                window.draw(block);
+            }
+        }
+
+    }
+
+    for (auto& entity : eTetrominos)
+    {
+        auto tetromino = game->getECSManager().getComponent<Tetromino>(entity);
+
+        if (!tetromino->next) continue;
+
+        auto xOffset = columns + 2;
+        auto yOffset = 4;
+
+        auto shape = shapes[tetromino->shapeIndex];
+        for (int x = 0; x < shapeSize; x++)
+        {
+            for (int y = 0; y < shapeSize; y++)
+            {
+                if (shape[y][x] == 0) continue;
+
+                sf::RectangleShape block(sf::Vector2f(cellSize, cellSize));
+                block.setFillColor(tetromino->color);
+                block.setPosition((x + xOffset) * cellSize, (y + yOffset) * cellSize);
+                window.draw(block);
+            }
+        }
+    }
+}
+
+void SceneTetris::renderGrid(sf::RenderWindow& window)
+{
+    auto width = game->getWidth();
+    auto height = game->getHeight();
+
+    for (int x = 0; x < cellSize * columns + cellSize; x += cellSize)
+    {
+        sf::Vertex line[] =
+        {
+            sf::Vertex(sf::Vector2f(x, 0)),
+            sf::Vertex(sf::Vector2f(x, cellSize * rows))
+        };
+
+        window.draw(line, 2, sf::Lines);
+    }
+
+    for (int y = 0; y < cellSize * rows + cellSize; y += cellSize)
+    {
+        sf::Vertex line[] =
+        {
+            sf::Vertex(sf::Vector2f(0, y)),
+            sf::Vertex(sf::Vector2f(cellSize * columns, y))
+        };
+
+        window.draw(line, 2, sf::Lines);
+    }
+}
--- ECSArcade/src/game/scene/tetris/SceneTetris.h
+++ ECSArcade/src/game/scene/tetris/SceneTetris.h
@@ -2,6 +2,7 @@
 
 #include "../common/Scene.h"
 #include "../../engine/Game.h"
+#include "../../common/Vec2.h"
 #include "../titlescreen/SceneTitleScreen.h"
 
 enum TetrisAction
@@ -10,6 +11,25 @@ enum TetrisAction
     TETRIS_DOWN,
     TETRIS_LEFT,
     TETRIS_RIGHT
+};
+
+struct Block
+{
+    sf::Color color{ 30, 30, 30 };
+    Vec2 position;
+};
+
+struct Tetromino {
+    sf::Color color;
+    int shapeIndex = 0;
+    Vec2 topLeftPos;
+    bool next = false;
+    bool active = false;
+};
+
+enum TetrisTag
+{
+    TETROMINO
 };
 
 class SceneTetris : public Scene
@@ -25,5 +45,64 @@ public:
 private:
     const sf::Color windowClearColor = sf::Color(0, 0, 0);
 
+    const static int columns = 10;
+    const static int rows = 20;
+    const int cellSize = 23;
+    const int movementCooldown = 30;
+    int currentMovementCooldown = 0;
+
+    const static int nrOfTetrominos = 7;
+    const static int shapeSize = 4;
+    const int shapes[nrOfTetrominos][shapeSize][shapeSize] = {
+        { { 0, 1, 0, 0 },
+          { 0, 1, 0, 0 },
+          { 0, 1, 0, 0 },
+          { 0, 1, 0, 0 } },
+
+        { { 0, 1, 1, 0 },
+          { 0, 1, 1, 0 },
+          { 0, 0, 0, 0 },
+          { 0, 0, 0, 0 } },
+
+        { { 1, 1, 1, 0 },
+          { 0, 1, 0, 0 },
+          { 0, 0, 0, 0 },
+          { 0, 0, 0, 0 } },
+
+        { { 0, 0, 1, 0 },
+          { 0, 0, 1, 0 },
+          { 0, 0, 1, 0 },
+          { 0, 1, 1, 0 } },
+
+        { { 0, 1, 0, 0 },
+          { 0, 1, 0, 0 },
+          { 0, 1, 0, 0 },
+          { 0, 1, 1, 0 } },
+
+        { { 0, 1, 1, 0 },
+          { 1, 1, 0, 0 },
+          { 0, 0, 0, 0 },
+          { 0, 0, 0, 0 } },
+
+        { { 1, 1, 0, 0 },
+          { 0, 1, 1, 0 },
+          { 0, 0, 0, 0 },
+          { 0, 0, 0, 0 } }
+    };
+
+    const Tetromino tetrominos[nrOfTetrominos]{
+        Tetromino { sf::Color(  0, 255, 255), 0 },
+        Tetromino { sf::Color(255, 255,   0), 1 },
+        Tetromino { sf::Color(255,   0, 255), 2 },
+        Tetromino { sf::Color(  0,   0, 255), 3 },
+        Tetromino { sf::Color(255, 129,   0), 4 },
+        Tetromino { sf::Color(  0, 255,   0), 5 },
+        Tetromino { sf::Color(255,   0,   0), 6 }
+    };
+
     void createActionList();
+    void renderBlocks(sf::RenderWindow& window);
+    void renderGrid(sf::RenderWindow& window);
+    void createNextTetromino();
+    void activateNextTetromino();
 };
